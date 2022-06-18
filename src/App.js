a25632a2f436b7b0d6d14df9b994423f4cf4766c
sergey.shapovalov <Shapovalov.Sergey2016@yandex.ru>
2022-06-18 19:45:51 +0300

    create application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// Link to Figma 
+// https://www.figma.com/file/qcmKp98gPYVyHQRSvVUxrX/Templates-%2322.-More-on-d-e-n.info?node-id=1%3A19
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
